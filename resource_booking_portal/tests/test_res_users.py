2dce03675d147ac324b1081a0fa35597e79952ec
Adasat Torres de León <a.torres@binhex.cloud>
2024-12-17 14:35:34 +0000

    [ADD]resourse_booking_portal

---
+++ resource_booking_portal/tests/test_res_users.py
@@ -0,0 +1,27 @@
+# Copyright 2024 Binhex - Adasat Torres de León
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo.tests.common import TransactionCase
+
+
+class TestResUsers(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.test_user = cls.env["res.users"].create(
+            {
+                "name": "test",
+                "login": "test",
+                "password": "test",
+                "groups_id": [(4, cls.env.ref("base.group_portal").id, 0)],
+                "create_booking_from_portal": False,
+            }
+        )
+
+    def test_is_portal_user(self):
+        self.test_user._compute_is_portal_user()
+        self.assertTrue(self.test_user.is_portal_user)
+
+    def test_create_booking_from_portal(self):
+        self.assertFalse(self.test_user.create_booking_from_portal)
+        self.test_user.create_booking_from_portal = True
+        self.assertTrue(self.test_user.create_booking_from_portal)
